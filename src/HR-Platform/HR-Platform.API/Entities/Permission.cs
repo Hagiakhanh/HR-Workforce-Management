912ead394100b6a8aad712e9728b5e17ef0db80b
Ha Gia Khanh <gkhanh903@gmail.com>
2026-08-19 22:20:40 +0700

    refactor code

--- src/HR-Platform/HR-Platform.API/Entities/Permission.cs
+++
@@ -1,9 +0,0 @@
-namespace HR_Platform.API.Entities;
-
-public class Permission
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string Code { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string Module { get; set; } = string.Empty;
-}
